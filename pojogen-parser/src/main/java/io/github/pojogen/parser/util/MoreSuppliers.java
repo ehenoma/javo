a0b770dc297f942a1fb0599f72d775a0f8ae999a
Merlin O <merlinosayimwen@gmail.com>
2018-05-23 16:31:36 +0200

    Documented the MoreSuppliers utility-class.

--- pojogen-parser/src/main/java/io/github/pojogen/parser/util/MoreSuppliers.java
+++ pojogen-parser/src/main/java/io/github/pojogen/parser/util/MoreSuppliers.java
@@ -2,11 +2,28 @@ package io.github.pojogen.parser.util;
 
 import java.util.function.Supplier;
 
+/**
+ * Provides additional factory methods that help working with the {@code {@link Supplier}} class.
+ * This class is more of an complement to {@code Guavas} utility-class.
+ *
+ * @author Merlin Osayimwen
+ * @see Supplier
+ * @since 1.0
+ */
 public final class MoreSuppliers {
 
+  /**
+   * Private constructor that prohibits construction of the utility-class.
+   */
   private MoreSuppliers() {
   }
 
+  /**
+   * Returns a supplier of the given type that always supplies {@code null}.
+   *
+   * @param <E> Type of the {@code {@link Supplier}}.
+   * @return Supplier of the given type that always supplies {@code null}.
+   */
   public static <E> Supplier<E> supplyNull() {
     return () -> null;
   }
