a92a068118dec973b1535abf0d5e458c4b3174d4
Merlin O <merlinosayimwen@gmail.com>
2018-05-23 13:30:39 +0200

    Added a custom test of the StructParser that takes input from the console.

--- pojogen-parser/src/test/java/com/github/merlinosayimwen/pojogen/StructParserTests.java
+++ pojogen-parser/src/test/java/com/github/merlinosayimwen/pojogen/StructParserTests.java
@@ -1,29 +1,44 @@
 package com.github.merlinosayimwen.pojogen;
 
+import java.util.Scanner;
+
+import com.google.common.base.Preconditions;
+
 import io.github.pojogen.parser.StructParser;
 import io.github.pojogen.parser.StructParserException;
 import io.github.pojogen.parser.StructParserFactory;
 import io.github.pojogen.struct.Struct;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 public class StructParserTests {
+
+  private static final String INPUT_READ_STOP_COMMAND = "/end";
 
   public static void main(final String... ignoredArguments) {
     final StructParser parser = new StructParserFactory().create();
 
-    try (final InputStream resource = new FileInputStream(new File(
-        Thread.currentThread().getClass().getClassLoader().getResource("example.struct")
-            .getPath()))) {
-      final Struct struct = parser.parseOne(resource);
+    try (final Scanner userInput = new Scanner(System.in)) {
+      final Struct struct = parser
+          .parseOne(StructParserTests.readInputUntil(userInput, INPUT_READ_STOP_COMMAND));
       System.out.println(struct);
-    } catch (final IOException resourceFailure) {
-      System.out.println("Could not read resource {example.struct}");
     } catch (final StructParserException parsingFailure) {
       parsingFailure.printStackTrace();
     }
+  }
+
+  private static String readInputUntil(final Scanner inputSource, final String stopCommand) {
+    Preconditions.checkArgument(inputSource.hasNextLine(), "The {inputSource} is empty");
+
+    final StringBuilder readInput = new StringBuilder();
+    for (String line = inputSource.nextLine(); line != null;
+        line = inputSource.nextLine()) {
+      if (stopCommand.equalsIgnoreCase(line)) {
+        break;
+      }
+
+      readInput.append(line);
+    }
+
+    return readInput.toString();
   }
 
 }
