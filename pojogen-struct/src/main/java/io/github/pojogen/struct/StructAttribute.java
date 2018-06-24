dd1524e2dc1e4ff1c0fabaa5415ad7a7983f71af
Merlin O <merlinosayimwen@gmail.com>
2018-06-24 16:02:43 +0200

    Make public constructor private, remove other private constructors and change the location of the static methods.

--- pojogen-struct/src/main/java/io/github/pojogen/struct/StructAttribute.java
+++ pojogen-struct/src/main/java/io/github/pojogen/struct/StructAttribute.java
@@ -38,99 +38,16 @@ public class StructAttribute {
   private final boolean constant;
 
   /**
-   * Parameterized constructor that takes a attribute.
-   *
-   * @param attribute Attribute that is copied.
-   */
-  private StructAttribute(final StructAttribute attribute) {
-    this(attribute.name, attribute.typeName, attribute.constant);
-  }
-
-  /**
-   * Parameterized constructor that initializes the instance with a name.
-   *
-   * @param name Name of the attribute.
-   */
-  private StructAttribute(final String name) {
-    this(name, Object.class.getSimpleName());
-  }
-
-  /**
-   * Parameterized constructor that initializes the instance with a name and typeName.
-   *
-   * @param name Name of the attribute.
-   * @param typeName Name of the attributes type.
-   */
-  private StructAttribute(final String name, final String typeName) {
-    this(name, typeName, false);
-  }
-
-  /**
    * Parameterized constructor that initializes the instance with all possible arguments.
    *
    * @param name Name of the attribute.
    * @param typeName Name of the attributes type.
    * @param constant Flag that indicates whether the attribute is final.
    */
-  public StructAttribute(final String name, final String typeName, final boolean constant) {
+  private StructAttribute(final String name, final String typeName, final boolean constant) {
     this.name = name;
     this.typeName = typeName;
     this.constant = constant;
-  }
-
-  /**
-   * Creates a StructAttribute from a name.
-   *
-   * @param name Name that will be given to the attribute.
-   * @return Created attribute with the {@code name} given.
-   */
-  public static StructAttribute create(final String name) {
-    Preconditions.checkNotNull(name);
-
-    return new StructAttribute(name);
-  }
-
-  /**
-   * Creates a StructAttribute from a name and with a typeName.
-   *
-   * @param name Name that will be given to the struct.
-   * @param typeName Name of the attributes type.
-   * @return Created attribute with the {@code name} and {@code typeName}.
-   */
-  public static StructAttribute create(final String name, final String typeName) {
-    Preconditions.checkNotNull(name);
-    Preconditions.checkNotNull(typeName);
-
-    return new StructAttribute(name, typeName);
-  }
-
-  /**
-   * Creates a StructAttribute with all possible arguments.
-   *
-   * @param name Name that will be given to the struct.
-   * @param typeName Name of the attributes type.
-   * @param constant Flag that indicates whether the attribute should be "final".
-   * @return Created attribute with all possible arguments set.
-   */
-  public static StructAttribute create(
-      final String name, final String typeName, final boolean constant) {
-
-    Preconditions.checkNotNull(name);
-    Preconditions.checkNotNull(typeName);
-
-    return new StructAttribute(name, typeName, constant);
-  }
-
-  /**
-   * Creates a copy of the given {@code struct}.
-   *
-   * @param struct Struct which is copied.
-   * @return Copy of the given {@code struct}.
-   */
-  public static StructAttribute copyOf(final StructAttribute struct) {
-    Preconditions.checkNotNull(struct);
-
-    return new StructAttribute(struct);
   }
 
   /**
@@ -195,5 +112,59 @@ public class StructAttribute {
   @Override
   public int hashCode() {
     return Objects.hash(this.name, this.typeName, this.constant);
+  }
+  /**
+   * Creates a StructAttribute from a name.
+   *
+   * @param name Name that will be given to the attribute.
+   * @return Created attribute with the {@code name} given.
+   */
+  public static StructAttribute create(final String name) {
+    Preconditions.checkNotNull(name);
+
+    return new StructAttribute(name, Object.class.getSimpleName(), false);
+  }
+
+  /**
+   * Creates a StructAttribute from a name and with a typeName.
+   *
+   * @param name Name that will be given to the struct.
+   * @param typeName Name of the attributes type.
+   * @return Created attribute with the {@code name} and {@code typeName}.
+   */
+  public static StructAttribute create(final String name, final String typeName) {
+    Preconditions.checkNotNull(name);
+    Preconditions.checkNotNull(typeName);
+
+    return new StructAttribute(name, typeName, false);
+  }
+
+  /**
+   * Creates a StructAttribute with all possible arguments.
+   *
+   * @param name Name that will be given to the struct.
+   * @param typeName Name of the attributes type.
+   * @param constant Flag that indicates whether the attribute should be "final".
+   * @return Created attribute with all possible arguments set.
+   */
+  public static StructAttribute create(
+      final String name, final String typeName, final boolean constant) {
+
+    Preconditions.checkNotNull(name);
+    Preconditions.checkNotNull(typeName);
+
+    return new StructAttribute(name, typeName, constant);
+  }
+
+  /**
+   * Creates a copy of the given {@code struct}.
+   *
+   * @param struct Struct which is copied.
+   * @return Copy of the given {@code struct}.
+   */
+  public static StructAttribute copyOf(final StructAttribute struct) {
+    Preconditions.checkNotNull(struct);
+
+    return new StructAttribute(struct.getName(), struct.getTypeName(), struct.isConstant());
   }
 }
