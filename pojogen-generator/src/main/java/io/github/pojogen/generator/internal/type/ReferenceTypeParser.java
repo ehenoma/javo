11abedb8b49090d26cbbb7fb32d85b63a5587316
Merlin O <merlinosayimwen@gmail.com>
2018-06-24 12:38:36 +0200

    Add ReferenceTypeParser to the generator.

--- pojogen-generator/src/main/java/io/github/pojogen/generator/internal/type/ReferenceTypeParser.java
+++ pojogen-generator/src/main/java/io/github/pojogen/generator/internal/type/ReferenceTypeParser.java
@@ -16,11 +16,62 @@
 
 package io.github.pojogen.generator.internal.type;
 
+import static java.util.regex.Pattern.compile;
+
+import com.google.common.base.Preconditions;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.annotation.Nullable;
+
 public final class ReferenceTypeParser {
+
+  private static final Pattern COLLECTION_PARSE_EXPRESSION =
+      compile("(\\w+)?(?:<(\\w+)>)|(\\[(\\w+)])");
 
   private ReferenceTypeParser() {}
 
   public ReferenceType parseReference(final String entry) {
+    Preconditions.checkNotNull(entry);
+
+    final Matcher parsed = ReferenceTypeParser.COLLECTION_PARSE_EXPRESSION.matcher(entry);
+    if (!parsed.matches()) {
+      // TODO: Implement abstract/concrete checks.
+      return PlainReferenceType.create(entry, false);
+    }
+
+    return this.parseCollectionType(parsed)
+        .orElseGet(() -> PlainReferenceType.create(entry, false));
+  }
+
+  private Optional<ReferenceType> parseCollectionType(final Matcher matcher) {
+    if (matcher.groupCount() != 4) {
+      throw new IllegalArgumentException("Invalid Input");
+    }
+    try {
+      if (matcher.group(1) != null && matcher.group(2) != null) {
+        return Optional.of(this.parseGenericCollectionType(matcher));
+      }
+    } catch (final RuntimeException failure) {
+      // Just continue... The exception does not need any handling
+      // since it's just an indicator that the groups are not available.
+    }
+
+    try {
+      if (matcher.group(3) != null && matcher.group(4) != null) {
+        return Optional.of(this.parseArrayType(matcher));
+      }
+    } catch (final RuntimeException failure) {
+      // This does not need handling neither.
+    }
+    return Optional.empty();
+  }
+
+  private ReferenceType parseArrayType(final Matcher matcher) {
+    return ArrayReferenceType.fromRawType(matcher.group(4));
+  }
+
+  private ReferenceType parseGenericCollectionType(final Matcher matcher) {
     return null;
   }
 
